cbdc23912ed5931f13902a080d5449ae077203b6
soliy <soliy@NoBoAnats>
2025-01-08 23:51:30 +0200

    news of personClasses

--- personclass/src/personclass/Main.java
+++ personclass/src/personclass/Main.java
@@ -1,5 +1,7 @@
 package personclass;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -33,6 +35,30 @@ public class Main {
 		pushkin.readPages(400);
 		shekspire.read();
 		shekspire.readPages(152);
+		
+		
+		//public static void main(String[] args){
+		    Scanner sc = new Scanner(System.in);
+		    System.out.println("Enter age of human");
+		    //enter from user:
+		    int age = (int)sc.nextInt();
+		    String ageGroup = "nun";
+		    // Determine age group based on age:
+		    	if(age<0) {
+		    		System.out.println("This age is not correct");
+		    	}else if(age>=0&&age<=2){
+		            ageGroup = "infant";
+		        }else if(age>=3&&age<=12){
+		            ageGroup = "child";
+		        }else if(age>=13&&age<=19){
+		            ageGroup = "teen";
+		        }else if(age>=20&&age<=64){
+		            ageGroup = "adult";
+		        }else if(age>=65){
+		            ageGroup = "senior";
+		        }
+		    System.out.println(ageGroup);
+		//}
 		
 	}
 
